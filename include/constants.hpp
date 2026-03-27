dafbb9013e1597ebe2115cfe0dc1879287798383
alexerby <mail@alexandereriksson.se>
2026-03-27 09:18:49 +0100

    refactor: mv color constants to constants.hpp

--- include/constants.hpp
+++ include/constants.hpp
@@ -1,9 +1,20 @@
 #pragma once
+#include <string>
 
 namespace constants {
 // 1e9 = 1 second
 // 60 * 1e9 = 1 minute
 constexpr double NANOS_PER_SECOND = 1'000'000'000.0;
 constexpr double NANOS_PER_MINUTE = 60'000'000'000.0;
+
+// Colors
+const std::string RED = "\033[1;31m";
+const std::string GRN = "\033[1;32m";
+const std::string YEL = "\033[1;33m";
+const std::string CYN = "\033[1;36m";
+const std::string RESET = "\033[0m";
+
+// Clear screen
+const std::string CLEAR = "\033[2J\033[1;1H";
 
 } // namespace constants
--- src/main.cpp
+++ src/main.cpp
@@ -5,18 +5,14 @@
 #include <iostream>
 #include <thread> // For sleep
 
+#include "constants.hpp"
 #include "market.hpp"
 #include "metadata.hpp"
-
 // TODO: Define different callbacks for investigating the data,
 // so we can pass these anonymous functions into our replay.
 
 int main() {
   const std::string file_path = "./data/fomc.dbn.zst";
-
-  // FOMC Release 2026-03-18 14:00:00 ET is 18:00:00 UTC
-  // Nanoseconds: 1773856800000000000
-  const uint64_t START_TIME_NS = 1773856800000000000;
 
   db::DbnFileStore file_store{file_path};
   Market market;
@@ -29,11 +25,6 @@ int main() {
   };
 
   auto record_callback = [&](const db::Record &record) {
-    // Time Filter: Skip everything before the jump point
-    if (record.Header().ts_event.time_since_epoch().count() < START_TIME_NS) {
-      return db::KeepGoing::Continue;
-    }
-
     if (auto *mbo = record.GetIf<db::MboMsg>()) {
       market.Apply(*mbo);
 
@@ -41,17 +32,10 @@ int main() {
         const auto &symbol = symbol_map.At(*mbo);
         auto bbo = market.AggregatedBbo(mbo->hd.instrument_id);
 
-        const std::string RED = "\033[1;31m";
-        const std::string GRN = "\033[1;32m";
-        const std::string YEL = "\033[1;33m";
-        const std::string CYN = "\033[1;36m";
-        const std::string RESET = "\033[0m";
-
-        // Clear screen (Optional)
-        std::cout << "\033[2J\033[1;1H";
-
-        std::cout << YEL << ">> " << symbol << " | "
-                  << db::ToIso8601(mbo->ts_recv) << RESET << "\n";
+        // Clear screen
+        std::cout << constants::CLEAR;
+        std::cout << constants::YEL << ">> " << symbol << " | ";
+        std::cout << db::ToIso8601(mbo->ts_recv) << constants::RESET << "\n";
 
         const std::vector<std::size_t> depths = {1, 3, 5, 10, 30, 50};
 
@@ -61,17 +45,20 @@ int main() {
           int pos = static_cast<int>(imb * bar_width);
 
           std::string label = "L" + std::to_string(d) + " Imbalance:";
-          std::cout << std::left << std::setw(15) << label << " [" << GRN
-                    << std::string(pos, '|') << RESET
-                    << std::string(bar_width - pos, '.') << "] " << std::fixed
-                    << std::setprecision(4) << imb << "\n";
+          std::cout << std::left << std::setw(15) << label << " ["
+                    << constants::GRN << std::string(pos, '|')
+                    << constants::RESET << std::string(bar_width - pos, '.')
+                    << "] " << std::fixed << std::setprecision(4) << imb
+                    << "\n";
         }
 
         std::cout << "\n";
         if (!bbo.second.IsEmpty())
-          std::cout << RED << "  ASK  " << RESET << bbo.second << "\n";
+          std::cout << constants::RED << "  ASK  " << constants::RESET
+                    << bbo.second << "\n";
         if (!bbo.first.IsEmpty())
-          std::cout << GRN << "  BID  " << RESET << bbo.first << "\n";
+          std::cout << constants::GRN << "  BID  " << constants::RESET
+                    << bbo.first << "\n";
 
         std::cout << std::string(60, '-') << std::endl;
 
